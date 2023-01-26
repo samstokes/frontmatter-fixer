2656be5538e412c9d1cb9c9b6f9dd58936288559
Sam Stokes <me@samstokes.co.uk>
2023-01-25 23:40:48 -0800

    add parse YAML

--- src/frontmatter.rs
+++ src/frontmatter.rs
@@ -1,4 +1,12 @@
+pub type Result<T> = serde_yaml::Result<T>;
+
 const RULE_LENGTH: usize = "---\n".len();
+
+pub fn parse(s: &str) -> (Option<Result<serde_yaml::Value>>, &str) {
+    let (raw_frontmatter, content) = parse_raw(s);
+    let frontmatter = raw_frontmatter.map(serde_yaml::from_str);
+    (frontmatter, content)
+}
 
 pub fn parse_raw(s: &str) -> (Option<&str>, &str) {
     // first line must begin frontmatter if present
@@ -45,30 +53,72 @@ hello: world
     const EXAMPLE_NO_YFM: &'_ str = "";
 
     #[test]
-    fn parses_example() {
+    fn parses_example_raw() {
         let (yfm, content) = parse_raw(EXAMPLE);
         assert_eq!(Some("hello: world\n"), yfm);
         assert_eq!("# Title\n", content);
     }
 
     #[test]
-    fn parses_empty_yfm() {
+    fn parses_empty_yfm_raw() {
         let (yfm, content) = parse_raw(EXAMPLE_EMPTY_YFM);
         assert_eq!(Some(""), yfm);
         assert_eq!("# Title\n", content);
     }
 
     #[test]
-    fn parses_only_yfm() {
+    fn parses_only_yfm_raw() {
         let (yfm, content) = parse_raw(EXAMPLE_ONLY_YFM);
         assert_eq!(Some("hello: world\n"), yfm);
         assert_eq!("", content);
     }
 
     #[test]
-    fn parses_no_yfm() {
+    fn parses_no_yfm_raw() {
         let (yfm, content) = parse_raw(EXAMPLE_NO_YFM);
         assert_eq!(None, yfm);
+        assert_eq!("", content);
+    }
+
+    #[test]
+    fn parses_example() {
+        let (yfm, content) = parse(EXAMPLE);
+        let yfm = yfm.expect("should be present").expect("should parse");
+
+        let mut expected = serde_yaml::Mapping::new();
+        expected.insert("hello".into(), "world".into());
+        let expected = serde_yaml::Value::Mapping(expected);
+
+        assert_eq!(expected, yfm);
+        assert_eq!("# Title\n", content);
+    }
+
+    #[test]
+    fn parses_empty_yfm() {
+        let (yfm, content) = parse(EXAMPLE_EMPTY_YFM);
+        let _ = yfm
+            .expect("should be present")
+            .expect_err("should not parse empty string");
+        assert_eq!("# Title\n", content);
+    }
+
+    #[test]
+    fn parses_only_yfm() {
+        let (yfm, content) = parse(EXAMPLE_ONLY_YFM);
+        let yfm = yfm.expect("should be present").expect("should parse");
+
+        let mut expected = serde_yaml::Mapping::new();
+        expected.insert("hello".into(), "world".into());
+        let expected = serde_yaml::Value::Mapping(expected);
+
+        assert_eq!(expected, yfm);
+        assert_eq!("", content);
+    }
+
+    #[test]
+    fn parses_no_yfm() {
+        let (yfm, content) = parse(EXAMPLE_NO_YFM);
+        assert!(yfm.is_none());
         assert_eq!("", content);
     }
 }
